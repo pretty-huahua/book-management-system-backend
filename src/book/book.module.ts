0584edaa2a09d7100521b9374358729f327aa06c
shishun <1304795069@qq.com>
2024-08-02 18:05:56 +0800

    第一次提交

---
+++ src/book/book.module.ts
@@ -0,0 +1,15 @@
+import { DbModule } from 'src/db/db.module';
+import { Module } from '@nestjs/common';
+import { BookService } from './book.service';
+import { BookController } from './book.controller';
+
+@Module({
+  imports: [
+    DbModule.register({
+      path: 'book.json',
+    }),
+  ],
+  controllers: [BookController],
+  providers: [BookService],
+})
+export class BookModule {}
